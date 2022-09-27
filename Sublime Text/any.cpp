fe32fa4311db558c116e3188676585c7660accd0
WadekarPrashant <wadekarprashant733@gmail.com>
2022-09-27 23:58:58 +0530

    push

---
+++ Sublime Text/any.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 0;
+
+int main(){
+	
+	#ifndef a
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+		
+	int a, b;
+	cin>>a>>b;
+	
+	cout<<a<<"\n";
+	cout<<b<<endl;
+	
+	cout<<a + b<<endl;
+	return 0;
+}
---
+++ Sublime Text/first.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{	int n;
+	cin>>n;
+
+	cout<<n+2;
+	
+	return 0;
+	
+	
+	
+	
+	
+}
+
+
+
+// ctrl + alt + b --> for running code.
+// ctrl + enter --> for making new testcases when output comes.
+// 
