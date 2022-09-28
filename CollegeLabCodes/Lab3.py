a69597bef5b7c071e1786d04c303e71fd58a0e11
WadekarPrashant <wadekarprashant733@gmail.com>
2022-09-28 23:59:13 +0530

    pythonLab3

---
+++ CollegeLabCodes/Lab3.py
@@ -0,0 +1,109 @@
+str="python"
+print(str)
+
+
+# results- python
+
+# str="python"
+# str[1]=q
+# print(str)
+#
+# results- error
+
+str1="python"
+print(str1)
+
+#del str
+print(str1)
+
+# results- error
+
+str1='Hello'
+str2='World!'
+#using+
+print('str1+str2=',str1+str2)
+
+#using*
+print('str*3=',str1*3)
+
+
+#Iterating through a string
+# count = 0
+# for x in 'Hello World'
+    # if(x=='l')
+        # count+=1
+# print(count,'letters found')
+
+
+# "MITwpu".upper()
+# results- MITWPU
+
+# "MITwpu".lower()
+# results- mitwpu
+
+# "This will split all words into a list".split()
+# results-['This','will','split','all','words','a','list']
+
+# .join(['This','will','join','all','words','a','string'])
+# results- This will join all words into a string
+# 
+# 'Welcome to MITWPU Pune'.find(co)
+# Results-3
+# 
+# 
+# num = 3
+# if num>0:
+    # print(num,"Number is positive")
+# print("This is always printed")
+# 
+# num = 3
+# if num>=0:
+    # print(num,"Number is positive or Zero")
+# else:
+    # print(num,"Number is negative")
+# 
+# for i in range(10):
+    # print(i)
+# results- 123456789
+    # 
+# 
+# for i in range(2,10,2):
+    # print(i)
+# results- 2468
+# 
+# for number in range(1,5):
+    # print("the current numbers are",number)
+# results-1234
+    # 
+# for number in range(1,7,2):
+    # print("the current numbers are",number)
+# results-135
+# 
+# 
+# numbers_list= [1,2,3,4,5,6,7]
+# 
+# for i in numbers_list:
+    # square=i*i
+    # print("The square of ",i,"is",square)
+    # 
+# 
+# numbers_list= [1,2,3,4,5,6,7]
+# 
+# for i in numbers_list:
+    # print(i)
+    # if i==5:
+        # break
+# print(i)
+# 
+# 
+# 
+# a=10
+# while True:
+    # a=a-1
+    # print(a)
+    # if a<7:
+        # break
+        # print('Done')
+# 
+    #    
+        # 
