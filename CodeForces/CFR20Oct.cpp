cbec9d60fa38116613d23b6da5f467d5680e6a95
WadekarPrashant <wadekarprashant733@gmail.com>
2022-11-25 03:45:17 +0530

    run

--- CodeForces/CFR20Oct.cpp
+++ CodeForces/CFR20Oct.cpp
@@ -1,11 +1,20 @@
-// int x,y;
-//     cin>>x>>y;
-//     int a,b;
-//     for(int i=1;i<=y;i++){
-//         cin>>a>>b;
-//     }
-//     if(y<x){
-//         cout<<"YES"<<endl;
-//     }else{
-//         cout<<"NO"<<endl;
-//     }
+
+#include<iostream>
+using namespace std;
+
+int main()
+{  
+     int x,y;
+    cin>>x>>y;
+    int a,b;
+    for(int i=1;i<=y;i++){
+        cin>>a>>b;
+    }
+    if(y<x){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+    
+    return 0;
+} 
