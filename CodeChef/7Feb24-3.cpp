ec853284322c662d5862b203d30b7cc0108202c9
WadekarPrashant <wadekarprashant733@gmail.com>
2024-02-07 22:44:40 +0530

    3rd question

---
+++ CodeChef/7Feb24-3.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <vector>
+#define for(i, n) for (int i = 0; i < n; i++)
+#define ll long long
+
+using namespace std;
+
+int main()
+{
+    /* vector<string> name(vector<int>& arr)
+    {
+    int n = arr.size();
+    } 	*/
+
+    ll testc;
+    cin >> testc;
+    while (testc--)
+    {
+
+        ll num;
+        ll num1;
+        cin >> num >> num1;
+        int add = num + num1;
+        // cout<< __gcd(num,num1)<<endl;
+        int k = num - 1;
+        cout << num1 / k << endl;
+    }
+
+    return 0;
+}
